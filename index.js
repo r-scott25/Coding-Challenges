455d85b929e1e082ab4967f667771af2e3dc464c
Rachel Scott <99931021+r-scott25@users.noreply.github.com>
2022-10-25 16:56:58 +0000

    Completed

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+// Have the function FirstFactorial(num) take the num parameter being passed and return the factorial of it. For example: if num = 4, then your program should return (4 * 3 * 2 * 1) = 24. For the test cases, the range will be between 1 and 18 and the input will always be an integer.
+// Examples
+// Input: 4
+// Output: 24
+// Input: 8
+// Output: 40320
 
-// --The wanted word is in lowercase.
-// --The crowd of letters is all in uppercase.
-// --Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
 
-
-function detectWord(str) {
-   return str.replace( /[^a-z]/g, '' );
-
+function firstFactorial(num){
+ if (num === 0 || num ===1)
+   return 1;
+  for (let i = num - 1; i >= 1; i--){
+    num = num * i;
+  }
+  
+  return num;
 }
-console.log(detectWord("UcUNFYGaFYFYGtNUH"));
 
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
-
-console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
+console.log(firstFactorial(0));
+console.log(firstFactorial(1));
+console.log(firstFactorial(4));
+console.log(firstFactorial(8));
