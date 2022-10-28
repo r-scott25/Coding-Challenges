f3116e45b712ec247f66f7231cfdf4faa198ced5
Rachel Scott <99931021+r-scott25@users.noreply.github.com>
2022-10-28 16:00:28 +0000

    completed

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+// Create a function that takes an array containing only numbers and return the first element.
 
-// --The wanted word is in lowercase.
-// --The crowd of letters is all in uppercase.
-// --Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
-
-
-function detectWord(str) {
-   return str.replace( /[^a-z]/g, '' );
-
+function getFirstValue(arr) {
+let first= arr[0]
+return arr[0]
 }
-console.log(detectWord("UcUNFYGaFYFYGtNUH"));
 
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+console.log(getFirstValue([1, 2, 3]));
 
-console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
+console.log(getFirstValue([80, 5, 100]));
+
+console.log(getFirstValue([-500, 0, 50]));
