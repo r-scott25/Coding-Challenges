23143bce129e93f4eeb68629bc636fd6d8f5642f
Rachel Scott <99931021+r-scott25@users.noreply.github.com>
2022-11-01 16:08:41 +0000

    completed challenge

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+// Write a function that returns the string "something" joined with a space " " and the given argument a.
 
-// --The wanted word is in lowercase.
-// --The crowd of letters is all in uppercase.
-// --Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
+function giveMeSomething(a) {
+	return "something " + a;
 
-
-function detectWord(str) {
-   return str.replace( /[^a-z]/g, '' );
-
+	
 }
-console.log(detectWord("UcUNFYGaFYFYGtNUH"));
 
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+console.log(giveMeSomething("is better than nothing"));
 
-console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
+console.log(giveMeSomething("Bob Jane"));
+
+console.log(giveMeSomething("something"));
