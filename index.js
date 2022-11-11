70bde79da886700130cbb7d76bb9a40b4bc535a6
Rachel Scott <99931021+r-scott25@users.noreply.github.com>
2022-11-11 18:36:11 +0000

    Completed

--- index.js
+++ index.js
@@ -1,16 +1,32 @@
-// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
-
-// --The wanted word is in lowercase.
-// --The crowd of letters is all in uppercase.
-// --Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
-
-
-function detectWord(str) {
-   return str.replace( /[^a-z]/g, '' );
-
+function getPlanetName(id){
+  var name;
+  switch(id){
+    case 1: name = 'Mercury';
+    break;
+    case 2: name = 'Venus';
+    break;
+    case 3: name = 'Earth';
+    break;
+    case 4: name = 'Mars';
+    break;
+    case 5: name = 'Jupiter';
+    break;
+    case 6: name = 'Saturn';
+    break;
+    case 7: name = 'Uranus';
+    break;
+    case 8: name = 'Neptune';
+    break;
+  }
+   
+  
+  return name;
 }
-console.log(detectWord("UcUNFYGaFYFYGtNUH"));
-
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
-
-console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
+console.log(getPlanetName(1));
+console.log(getPlanetName(2));
+console.log(getPlanetName(3));
+console.log(getPlanetName(4));
+console.log(getPlanetName(5));
+console.log(getPlanetName(6));
+console.log(getPlanetName(7));
+console.log(getPlanetName(8));
