1cfc7833eaf23501c9c316a20efd91190cb0e426
Rachel Scott <99931021+r-scott25@users.noreply.github.com>
2022-12-06 16:29:43 +0000

    even or odd

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
-// A word is on the loose and now has tried to hide amongst a crowd of tall letters! Help write a function to detect what the word is, knowing the following rules:
+// Create a function that takes an integer as an argument and returns "Even" for even numbers or "Odd" for odd numbers.
 
-// --The wanted word is in lowercase.
-// --The crowd of letters is all in uppercase.
-// --Note that the word will be spread out amongst the random letters, but their letters remain in the same order.
-
-
-function detectWord(str) {
-   return str.replace( /[^a-z]/g, '' );
-
+function evenOrOdd(number) {
+  if (number % 2 === 0){
+    return "Even";
+  } else {
+    return "Odd";
+  }
 }
-console.log(detectWord("UcUNFYGaFYFYGtNUH"));
 
-console.log(detectWord("bEEFGBuFBRrHgUHlNFYaYr"));
+console.log(evenOrOdd(2));
+console.log(evenOrOdd(7));
+console.log(evenOrOdd(-524));
+console.log(evenOrOdd(1000));
+console.log(evenOrOdd(723));
+console.log(evenOrOdd(-421));
 
-console.log(detectWord("YFemHUFBbezFBYzFBYLleGBYEFGBMENTment"));
+
